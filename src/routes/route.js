b11729611dc8bc5f26b0158d0bdf74ad5de64fcd
hritik-dubey <76241469+hritik-dubey@users.noreply.github.com>
2022-05-20 00:37:13 +0530

    Update route.js

--- src/routes/route.js
+++ src/routes/route.js
@@ -12,11 +12,3 @@ router.get("/:urlCode",urlcontroler.getUrl)
 
 module.exports = router;
 
-// const express = require("express")
-// const router = express.Router()
-// const urlController =require("../controllers/urlController")
-
-// router.post("/url/shorten", urlController.createUrl)
-// router.get("/:urlCode", urlController.getUrl)
-
-// module.exports=router
